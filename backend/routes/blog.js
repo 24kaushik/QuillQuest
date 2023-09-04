af4d529a67a437ae095ba9a598e09a4e5c399454
Kaushik <akaushikarkar@gmail.com>
2023-09-04 20:47:26 +0530

    get blog by id endpoint completed

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -8,10 +8,24 @@ const Router = express.Router();
 // Route 0: Test route. /test 
 Router.use('/test', (req, res) => { res.status(200).send('This route is working.') })
 
+// Route 1: Fetching all the Blogs using: /fetchall . No login required.
+Router.get('/fetchall', async (req, res) => {
+    try {
+        // Fetching all the blogs
+        const blogs = await Blog.find();
+        res.status(200).json({ success: true, blogs })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ success: false, error: "Internal server error!" })
+    }
+})
+
 //Route 2: Creating a Blog using: /create . Login required
 Router.post('/create', fetchUser, [
     body('title', 'Title must be atleast 4 characters long!').isLength({ min: 4 }),
-    body('content', 'Content must be atleast 10 characters long').isLength({ min: 10 })
+    body('title', 'Title must be shorter than 50 characters!').isLength({ max: 50 }),
+    body('content', 'Content must be atleast 10 characters long!').isLength({ min: 10 }),
+    body('content', 'Content must be shorter than 4000 characters!').isLength({ max: 4000 })
 ], async (req, res) => {
     try {
         const { title, content } = req.body;
@@ -20,7 +34,7 @@ Router.post('/create', fetchUser, [
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
-            return res.status(400).json({ success: false, error: errors.array })
+            return res.status(400).json({ success: false, error: errors.array() })
         }
 
         // Checking if the blog is duplicate.
@@ -41,6 +55,23 @@ Router.post('/create', fetchUser, [
     } catch (error) {
         console.log(error.message)
         res.status(500).json({ success: false, error: "Internal server error!" })
+    }
+
+})
+
+// Route 3: Get Blog by id using: /getblog:id
+Router.get('/getblog/:id', async (req, res) => {
+    try {
+        //Searching the blog using the id given in uri
+        const blogID = req.params.id;
+        const blog = await Blog.findById(blogID).select();
+
+        if(blog){
+            return res.status(200).json({success: true, blog})
+        }
+
+    } catch (error) {
+        res.status(404).json({success:false, error: `No blog found with the id ${req.params.id}`})
     }
 
 })
