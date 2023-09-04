0a2b5bd5e3516fb09bf9aacd64b76959c31cef19
Kaushik <akaushikarkar@gmail.com>
2023-09-04 11:57:51 +0530

    login endpoint completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const Router = express.Router();
-const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+require('dotenv').config();
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const Router = express.Router();
 
 
 
@@ -12,14 +12,12 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 
 //Route 0: Test route: /auth/test . No login required.
-
 Router.use('/test', (req, res) => {
     res.status(200).send("This route is working")
 })
 
 
 //Route 1: Create user using: /auth/createuser . No login required.
-
 Router.post("/createuser", [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('username', 'Enter a valid username').isLength({ min: 3 }),
@@ -64,17 +62,63 @@ Router.post("/createuser", [
         const authToken = jwt.sign(data, JWT_SECRET);
         success = true;
 
-        res.status(200).json({success, authToken})
+        res.status(200).json({ success, authToken })
 
     } catch (error) {
         success = false
         console.log(error.message)
-        return res.status(500).json({success, error: "Internal server error!"});
+        return res.status(500).json({ success, error: "Internal server error!" });
+    }
+})
+
+// Route 2: Authenticate the user using: /login . No login required.
+Router.post('/login', [
+    body('username', 'Username cannot be blank').exists(),
+    body('password', "Password cannot be blank").exists()
+], async (req, res) => {
+
+    let success = false;
+
+    // Checking if name, username, password fits the criteria
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
     }
 
+    try {
+        // Finding user in the database (if exists) and returning the auth token
+        let {username, password} = req.body;
 
+        let user = await User.findOne({ username: username });
 
+        if (!user) {
+            success = false;
+            return res.status(401).json({ success, error: "Please try to login with correct credentialss!" });
+        }
 
+        const passwordCompare = await bcrypt.compare(password, user.password);
+
+        if (!passwordCompare) {
+            success = false;
+            return res.status(401).json({ success, error: "Please try to login with correct credentials!" });
+        }
+
+        // Creating an auth-token from user id
+        const data = {
+            user: { id: user.id }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET)
+        success = true
+        res.status(200).json({ success, authToken })
+
+    } catch (error) {
+        success = false;
+        console.log(error.message)
+        res.status(500).json({ success, error: "Internal server error!" })
+    }
 
 })
 
