888471e36b708ffcc218795966f6723077e67cf7
Kaushik <akaushikarkar@gmail.com>
2023-09-05 11:33:27 +0530

    Initilalized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-7xl text-teal-600'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
