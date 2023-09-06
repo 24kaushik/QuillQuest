e67e65b22000156138514e1d2d59ecbb1373415b
Kaushik <akaushikarkar@gmail.com>
2023-09-06 21:54:40 +0530

    Added alert component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
@@ -8,16 +8,32 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Login from './components/Login';
+import Alert from './components/Alert';
 
 function App() {
+
+  const [alert, setAlert] = useState({msg:"", type:''});
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert({msg:"", type:''});
+    }, 2500);
+  }
 
   return (
     <>
       <Router>
         <Navbar />
+        <Alert alert={alert} />
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/write' element={<Writeblog />} />
+          <Route exact path='/' element={<Home showAlert={showAlert} />} />
+          <Route exact path='/write' element={<Writeblog showAlert={showAlert} />} />
+          <Route exact path='/login' element={<Login showAlert={showAlert} />} />
         </Routes>
         <Footer />
       </Router>
