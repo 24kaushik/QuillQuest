365d3c7b024b28126954a41247c76c817746fb24
Kaushik <akaushikarkar@gmail.com>
2023-09-10 14:57:12 +0530

    removed close button that wasnt visible

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -168,7 +168,6 @@ const Profile = props => {
                             <div className="modal-content">
                                 <div className="modal-header">
                                     <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Blog</h1>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div className="modal-body">
                                     <div className="mb-3">
